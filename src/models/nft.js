f469dfed69d996fa69d9a8e0e418ccd765cbeb17
Rounak Dubey <rkdy49@gmail.com>
2023-06-25 02:32:36 +0530

    Update nft.js

--- src/models/nft.js
+++ src/models/nft.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
-const db = require('../db')
+const mongoose = require('mongoose');
+const db = require('../db');
 
 const schema = new mongoose.Schema({
   tokenId: {
     type: Number,
-    required: [true, 'A collection must have an address'],
-    unique: true,
+    required: [true, 'A collection must have a tokenId'],
   },
   contractAddress: {
     type: Number,
+    required: [true, 'A collection must have a contractAddress'],
   },
   metadata: {
     type: JSON,
@@ -25,7 +25,11 @@ const schema = new mongoose.Schema({
   chain: {
     type: String,
   },
-})
-const nft = mongoose.model('nfts', schema)
+});
 
-module.exports = nft
+// Define a compound index on tokenId and contractAddress
+schema.index({ tokenId: 1, contractAddress: 1 }, { unique: true });
+
+const nft = mongoose.model('nfts', schema);
+
+module.exports = nft;
