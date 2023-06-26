dfe2c138a0561abf471c2c82fe48976234139b61
Rachit Agarwal <rachitag112@gmail.com>
2023-06-27 01:11:09 +0530

    Changes to route

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const assetsRouter = require('./src/assetsRoutes')
 const db = require('./src/db')
 
 const app = express()
+const router = express.Router();
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', assetsRouter)
+
+router.get("/", (req, res) => {
+  res.send("GearFi Rocks!");
+});
 
 const port = process.env.PORT
 app.listen(port, () => {
